d012bf973fbd9eea27b35f704d06346751608739
laurenceKohli <laurence.kohli@hes-so.com>
2025-01-08 13:59:24 +0100

    Changement types Lat/Long

--- seeders/postesSeeder.js
+++ seeders/postesSeeder.js
@@ -3,19 +3,19 @@ import Poste from '../models/poste.js';
 export function seedPostes() {
     const postes = [
         {
-            geoloc: { lat: 2539171.85, long: 1181359.59 },
+            geoloc: { lat: 46.780534, long: 6.642302 },
             number: 30,
             images: ['image1.jpg', 'image2.jpg'],
             descr: 'Poste 1 (au coin de la Banque)'
         },
         {
-            geoloc: { lat: 2539373.70, long: 1181492.65 },
+            geoloc: { lat: 46.780906, long: 6.643686 },
             number: 31,
             images: ['image3.jpg', 'image4.jpg'],
             descr: 'Poste 2 (sur le pont)'
         },
         {
-            geoloc: { lat: 2539490.81, long: 1181426.72 },
+            geoloc: { lat: 46.781278, long: 6.646287 },
             number: 32,
             images: ['image5.jpg', 'image6.jpg'],
             descr: 'Poste 3 (devant la porte)'
